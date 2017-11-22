bed66125c5a8ae9c575168b3b9d3eec5e391c529
fccbubblun <forrestwilliams7c4@gmail.com>
2017-11-22 11:30:49 -0800

    added basic image search functionality

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+require('dotenv').config();
 var express = require('express');
+var google = require('googleapis');
+var customsearch = google.customsearch('v1');
 var app = express();
 
 app.use(express.static('public'));
 
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/description.html');
-});
-
-app.get("/whoami", function (request, response) {
-  var json = {"ip": request.headers["x-forwarded-for"] || request.ip, "language": request.headers["accept-language"], "software": request.headers["user-agent"]};
-  response.send(json);
+  customsearch.cse.list(
+    {cx: process.env.CX, q: "hello world", auth: process.env.GAUTH, searchType: "image"},
+    function(err, resp){
+      if(err){
+        return console.log("Error:", err);
+      }
+      console.log("Response:" + resp.searchInformation.formattedTotalResults);
+      if (resp.items && resp.items.length > 0) {
+        console.log('First result is ' + JSON.stringify(resp.items[0]));
+      }
+    });
+    response.sendFile(__dirname + '/views/description.html');
 });
 
 // listen for requests :)
